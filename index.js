221b84b10563e2c41b1a7c5b64533693c10c748b
Jared <Jared Allard>
2016-03-15 11:37:42 -0700

    forceKill, better error handling

--- index.js
+++ index.js
@@ -28,13 +28,23 @@ app.use(morgan('dev'));
 
 // check the authentication
 function checkAuth(req, res, next) {
+  if(!req.get('Authentication')) {
+    return res.send({
+      success: false,
+      reason: 'NOAUTH'
+    });
+  }
+
   let apikey = req.get('Authentication').replace('Basic ', '');
   let authparsed = apikey.split(':');
+
+  // split variables.
   let accessToken = authparsed[0];
   let accessTokenSecret = authparsed[1];
 
   let success = false;
 
+  // check if the autentication is, well, authenticated.
   if(config.authentication !== undefined) {
     let isAuthenticated = config.authentication.filter(function(o) {
       if(o.accessToken === accessToken && o.accessTokenSecret === accessTokenSecret) {
@@ -42,20 +52,23 @@ function checkAuth(req, res, next) {
       }
     })[0];
 
+    // if we didn't find the token, it's not authenticated.
     if(isAuthenticated !== undefined) {
       success = true;
     }
   }
 
-  if(success) {
-    next();
-  } else {
+  // if not success, fail.
+  if(!success) {
     console.log('[mcf-api] authentication failure')
     return res.send({
       success: false,
       reason: 'INVALIDAUTH'
     });
   }
+
+  // continue.
+  return next();
 }
 
 /**
@@ -84,14 +97,41 @@ app.get('/server/status', function(req, res) {
 app.get('/server/stop', checkAuth, function(req, res) {
   let start = Date.now();
 
-  M.sendCommand('stop', function(resp) {
+  M.sendCommand('stop', function(e) {
     let lat = Date.now() - start;
+
+    if(!e) {
+      return res.send({
+        latency: lat,
+        success: false,
+        reason: 'NOTRUNNING'
+      });
+    }
+
     res.send({
       latency: lat,
-      success: resp
+      success: e
     });
   });
 });
+
+/**
+ * GET /server/forceStop
+ *
+ * Kill the server for sure.
+ **/
+ app.get('/server/forceStop', checkAuth, function(req, res) {
+   let start = Date.now();
+
+   M.forceKill(function() {
+     let lat = Date.now() - start;
+
+     res.send({
+       latency: lat,
+       success: true
+     });
+   });
+ });
 
 /**
  * GET /server/start
@@ -108,6 +148,33 @@ app.get('/server/start', checkAuth, function(req, res) {
       success: err
     });
   });
-})
+});
 
-app.listen(8080);
+/**
+ * GET /server/sendCommand/
+ **/
+app.get('/server/sendCommand/:command', checkAuth, function(req, res) {
+  let command = decodeURIComponent(req.params.command);
+  let start = Date.now();
+
+  // send the command.
+  M.sendCommand(command, function(e) {
+    let lat = Date.now() - start;
+
+    if(!e) {
+      return res.send({
+        latency: lat,
+        success: false,
+        reason: 'NOTRUNNING'
+      });
+    }
+
+    res.send({
+      latency: lat,
+      success: e,
+      command: command
+    });
+  });
+});
+
+app.listen(config.port);
